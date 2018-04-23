c2bdb3e0130616e54178b9914eac49fd3ca1c982
goldenLiang <870385737@qq.com>
2018-04-23 15:13:00 +0800

    MySQL optimistic lock

--- src/main/java/com/lc/seckill/controller/SecKillController.java
+++ src/main/java/com/lc/seckill/controller/SecKillController.java
@@ -1,30 +1,75 @@
 package com.lc.seckill.controller;
 
+import com.lc.seckill.cache.RedisCacheHandle;
 import com.lc.seckill.common.Message;
 import com.lc.seckill.common.SecKillEnum;
+import com.lc.seckill.service.SecKillService;
 import com.lc.seckill.web.req.SecKillRequest;
 import com.lc.seckill.web.vo.SecKillResponse;
-import com.lc.seckill.service.SecKillService;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
-@RequestMapping("seckill")
+@RequestMapping("/seckill")
 @RestController
 public class SecKillController {
 
     @Autowired
     private SecKillService secKillService;
 
+    /**
+     * MySQL悲观锁
+     * @param requestMessage
+     * @return
+     */
     @RequestMapping(value = "/pessLockInMySQL",method = RequestMethod.POST)
     public Message<SecKillResponse> pessLockInMySQL(@RequestBody Message<SecKillRequest> requestMessage){
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("username",requestMessage.getBody().getUsername());
+        paramMap.put("userId",requestMessage.getBody().getUserId());
         paramMap.put("productId",requestMessage.getBody().getProductId());
         SecKillEnum secKillEnum = secKillService.handleByPessLockInMySQL(paramMap);
-        Message<SecKillResponse> responseMessage = new Message<>(secKillEnum,new SecKillResponse());
+        Message<SecKillResponse> responseMessage = new Message<>(secKillEnum,null);
         return responseMessage;
+    }
+
+    /**
+     * MySQL乐观锁
+     * @param requestMessage
+     * @return
+     */
+    @RequestMapping(value = "/posiLockInMySQL",method = RequestMethod.POST)
+    public Message<SecKillResponse> posiLockInMySQL(@RequestBody Message<SecKillRequest> requestMessage){
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("userId",requestMessage.getBody().getUserId());
+        paramMap.put("productId",requestMessage.getBody().getProductId());
+        SecKillEnum secKillEnum = secKillService.handleByPosiLockInMySQL(paramMap);
+        Message<SecKillResponse> responseMessage = new Message<>(secKillEnum,null);
+        return responseMessage;
+    }
+
+    /**
+     * 利用redis的watch监控的特性
+     * @throws InterruptedException
+     */
+    @RequestMapping(value = "/baseOnRedisWatch",method = RequestMethod.POST)
+    public Message<SecKillResponse> baseOnRedisWatch(@RequestBody Message<SecKillRequest> requestMessage) throws InterruptedException {
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("userId",requestMessage.getBody().getUserId());
+        paramMap.put("productId",requestMessage.getBody().getProductId());
+        SecKillEnum secKillEnum = secKillService.handleByRedisWatch(paramMap);
+        Message<SecKillResponse> responseMessage = new Message<>(secKillEnum,null);
+        return responseMessage;
+    }
+
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    public String test(@RequestBody Message<SecKillRequest> requestMessage){
+        System.out.println(requestMessage.getBody().getUserId()+","+requestMessage.getBody().getProductId());
+        return "success";
     }
 }
